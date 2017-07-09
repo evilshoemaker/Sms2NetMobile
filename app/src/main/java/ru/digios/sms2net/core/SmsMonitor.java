6bc700a7d7aed213e2ce23d9addba344579499de
kow <kow@em70.ru>
2017-07-09 19:58:30 +0700

    Налаживаем прием сообщений

--- app/src/main/java/ru/digios/sms2net/core/SmsMonitor.java
+++ app/src/main/java/ru/digios/sms2net/core/SmsMonitor.java
@@ -3,13 +3,40 @@ package ru.digios.sms2net.core;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.telephony.SmsMessage;
+import android.util.Log;
+import android.widget.Toast;
 
 public class SmsMonitor extends BroadcastReceiver {
     private static final String ACTION = "android.provider.Telephony.SMS_RECEIVED";
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        Log.i("INFO.SmsMonitor", intent.getAction());
+        if (intent != null && intent.getAction() != null && ACTION.compareToIgnoreCase(intent.getAction()) == 0) {
+        //---получить входящее SMS сообщение---
+        Bundle bundle= intent.getExtras();
+        SmsMessage[] msgs=null;
+        String str="";
+        if(bundle!=null)
+        {
+//---извлечь полученное SMS ---
+            Object[] pdus=(Object[]) bundle.get("pdus");
+            msgs = new SmsMessage[pdus.length];
+            for(int i=0; i<msgs.length; i++){
+                msgs[i]= SmsMessage.createFromPdu((byte[])pdus[i]);
+                str+="SMS from "+ msgs[i].getOriginatingAddress();
+                str+=" :";
+                str+= msgs[i].getMessageBody().toString();
+                str+="\n";
+            }
+//---Показать новое SMS сообщение---
+            Toast.makeText(context, str, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
 //        if (intent != null && intent.getAction() != null &&
 //                ACTION.compareToIgnoreCase(intent.getAction()) == 0) {
 //            Object[] pduArray = (Object[]) intent.getExtras().get("pdus");
@@ -24,6 +51,7 @@ public class SmsMonitor extends BroadcastReceiver {
 //                    bodyText.append(messages[i].getMessageBody());
 //                }
 //                String body = bodyText.toString();
+//                Log.i("INFO.SmsMonitor", body);
 //
 //                /*Intent mIntent = new Intent(context, SmsService.class);
 //                mIntent.putExtra("sms_body", body);
